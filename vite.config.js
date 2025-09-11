8d320cfc39faa4910bf0471c71c3499fb6503e51
Manish Kushwah <manishsinghkushwah612@gmail.com>
2025-09-11 16:13:56 +0530

    a

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/Election/',
+// })
 export default defineConfig({
-  plugins: [react()],
-  base: '/Election/',
-})
+  plugins: [React()],
+  base: "/Election/", // 👈 yaha apna repo ka naam likho
+});
